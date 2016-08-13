e008857d4f648b695810d55012472732621b29d1
阿部司 <destaque1216@gmail.com>
2016-08-13 21:28:56 +0900

    更に修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
 end
