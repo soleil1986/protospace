5fe9f2e925db98bfc97a3f79c78413de3769cffe
阿部司 <destaque1216@gmail.com>
2016-08-15 18:17:33 +0900

    validatesの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:email]
+  validates :username, presence: true
+  validates :image, presence: true
+  validates :password, presence: true
+  validates :member, presence: true
+  validates :profile, presence: true
+  validates :works, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  get '/users/:id/edit' => 'users#edit'
   resources :users, only: [:show, :edit, :update]
 end
