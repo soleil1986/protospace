c6f44ac4c79765737cb6cba4cbbd6e5fd1d5a611
阿部司 <destaque1216@gmail.com>
2016-08-15 17:26:13 +0900

    ユーザー編集機能　追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   get '/users/:id/edit' => 'users#edit'
-  resources :users
+  resources :users, only: [:show, :edit, :update]
 end
