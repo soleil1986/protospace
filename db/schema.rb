af19bcfcb20081f0441c92b83ab39b0721dcfab7
阿部司 <destaque1216@gmail.com>
2016-08-20 20:18:59 +0900

    likeモデル及びテーブル、likesコントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816034810) do
+ActiveRecord::Schema.define(version: 20160820110343) do
 
   create_table "images", force: :cascade do |t|
     t.text     "image",        limit: 65535
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160816034810) do
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "prototypes", force: :cascade do |t|
