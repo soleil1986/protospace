e809fbdc610c300e78a7fec4bcf63a5d7ae536fe
阿部司 <destaque1216@gmail.com>
2016-08-23 21:59:26 +0900

    commentsのroutes記述、及びCommentsControllerにcreateアクション作成。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to controller: :prototypes, action: :show, id: params[:prototype_id]
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: [:new, :create]
+  end
   post '/like/:prototype_id' => 'likes#like', as: 'like'
   delete '/unlike/:prototype_id' => 'likes#unlike', as: 'unlike'
   root 'prototypes#index'
