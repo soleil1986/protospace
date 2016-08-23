c0743b6bceb4c051d577fcfaf720194812ce95c5
阿部司 <destaque1216@gmail.com>
2016-08-23 15:59:07 +0900

    commentモデル、及びテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820115026) do
+ActiveRecord::Schema.define(version: 20160823065359) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment",      limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.text     "image",        limit: 65535
