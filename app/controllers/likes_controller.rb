dd8b7f655376caaee36a7e115da719709a8411a7
阿部司 <destaque1216@gmail.com>
2016-08-23 14:52:13 +0900

    いいね機能修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,13 @@ class LikesController < ApplicationController
     @prototype = Prototype.find(params[:prototype_id])
     like = current_user.likes.build(prototype_id: @prototype.id)
     like.save
+    @prototype.reload
   end
 
   def unlike
     @prototype = Prototype.find(params[:prototype_id])
     like = current_user.likes.find_by(prototype_id: @prototype.id)
     like.destroy
+    @prototype.reload
   end
 end
