5f8a5c917834630aa64a6355525026491ed02527
阿部司 <destaque1216@gmail.com>
2016-08-25 19:42:15 +0900

    NewestsControllerとPopularsControllerにrender template: "prototypes/index"の設定。

--- app/controllers/newests_controller.rb
+++ app/controllers/newests_controller.rb
@@ -1,5 +1,6 @@
 class NewestsController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user).all.order("created_at DESC").page(params[:page]).per(8)
+    render template: "prototypes/index"
   end
 end
--- app/controllers/populars_controller.rb
+++ app/controllers/populars_controller.rb
@@ -1,5 +1,6 @@
 class PopularsController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user).all.order("likes_count DESC").page(params[:page]).per(8)
+    render template: "prototypes/index"
   end
 end
