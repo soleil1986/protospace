d36eac6c6ecfdb76bfc297e77fb6be054be8f49f
阿部司 <destaque1216@gmail.com>
2016-08-25 20:09:13 +0900

    delete newests model, delete populars model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825082050) do
+ActiveRecord::Schema.define(version: 20160823065359) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment",      limit: 65535
@@ -31,18 +31,6 @@ ActiveRecord::Schema.define(version: 20160825082050) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
-    t.integer  "prototype_id", limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  create_table "newests", force: :cascade do |t|
-    t.integer  "prototype_id", limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  create_table "populars", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
