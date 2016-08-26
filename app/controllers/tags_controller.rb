2b3168370f9092af68a26d59b2f926ebad291261
阿部司 <destaque1216@gmail.com>
2016-08-26 14:42:07 +0900

    tags_controller showアクション生成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,10 @@
 class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
- end
+  end
+
+  def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(@tag.name)
+  end
 end
