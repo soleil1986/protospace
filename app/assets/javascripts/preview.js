b6ac7cc540df36be95f0dcc95b214450b1016570
阿部司 <destaque1216@gmail.com>
2016-08-31 00:21:21 +0900

    インデントの修正

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,18 +1,18 @@
 $(document).on("ready page:load", function(){
-    $('.preview').each(function(){
+    $('.js-preview').each(function(){
         var preview = $(this),
-        image = preview.find('.image');
+        image = preview.find('.js-image');
         image.change(
-            function() {
-                if ( !this.files.length ) {
-                    return;
-                }
-                    var file = this.files[0]
-                    var fr = new FileReader();
-                    fr.readAsDataURL(file);
-                    fr.onload = function() {
-                        preview.css({'background-image':'url(' + fr.result + ')', 'background-size':'cover'});
-                    }
+            function(){
+            if ( !this.files.length ) {
+                return;
+            }
+            var file = this.files[0]
+            var fr = new FileReader();
+            fr.readAsDataURL(file);
+            fr.onload = function(){
+                preview.css({'background-image':'url(' + fr.result + ')', 'background-size':'cover'});
+            }
             }
         );
     });
