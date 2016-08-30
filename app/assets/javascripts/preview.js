c7ec4a55f368d48951edad39601a330c183dddd1
阿部司 <destaque1216@gmail.com>
2016-08-30 20:54:48 +0900

    プレビュー機能の実装

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,13 +1,19 @@
-$('#file').click(function(){
-    $('#file').change(function () {
-        var file = this.files[0];
-        var reader = new FileReader();
-        reader.onloadend = function () {
-           $('.preview').css('background-image', 'url("' + reader.result + '")');
-        }
-        if (file) {
-            reader.readAsDataURL(file);
-        } else {
-        }
+$(document).on("ready page:load", function(){
+    $('.preview').each(function(){
+        var preview = $(this),
+        image = preview.find('.image');
+        image.change(
+            function() {
+                if ( !this.files.length ) {
+                    return;
+                }
+                    var file = this.files[0]
+                    var fr = new FileReader();
+                    fr.readAsDataURL(file);
+                    fr.onload = function() {
+                        preview.css({'background-image':'url(' + fr.result + ')', 'background-size':'cover'});
+                    }
+            }
+        );
     });
 });
