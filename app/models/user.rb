efd30fa3873dbaca4686a613330752e62a3761a7
阿部司 <destaque1216@gmail.com>
2016-09-02 15:56:02 +0900

    記述修正、及び不要なvalidate削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  mount_uploader :avatar, ImageUploader
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:email]
   has_many :prototypes
   has_many :likes
   has_many :comments
-  validates_presence_of :username, :avatar, :password, :member, :profile, :works
+  validates_presence_of :username, :member, :profile, :works
 end
