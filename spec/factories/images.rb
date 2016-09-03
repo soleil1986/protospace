d9bfa790a0df83e5cefb4db59c8e6106f66f61d5
阿部司 <destaque1216@gmail.com>
2016-09-03 21:56:08 +0900

    image_spec.rbにformatが違うものを投稿した時のテストも追記

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -3,5 +3,4 @@ FactoryGirl.define do
   factory :image do
     image { Rack::Test::UploadedFile.new("spec/files/MtFuji.jpg", "image/jpg") }
   end
-
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -16,5 +16,13 @@ describe Image do
         expect(image).to be_valid
       end
     end
+
+    context 'without valid attributes' do
+      it "has the wrong image format" do
+        image = build(:image, image: Rack::Test::UploadedFile.new("spec/files/MtFuji.jpn", "image/jpn"))
+        image.valid?
+        expect(image.errors[:image]).to include("You are not allowed to upload \"jpn\" files, allowed types: jpg, jpeg, gif, png", "can't be blank")
+      end
+    end
   end
 end
